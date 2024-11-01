0e22af733723e83e0832ebc0e3f67fbdab742bf0
hieuTran <tranminhhieu291104@gmail.com>
2024-11-01 17:25:38 +0700

    inital

---
+++ MvcBookStore/Controllers/BookStoreController.cs
@@ -0,0 +1,74 @@
+﻿using MvcBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBookStore.Controllers
+{
+    public class BookStoreController : Controller
+    {
+        QLBANSACHEntities data = new QLBANSACHEntities();
+        private List<SACH> Laysachmoi(int count)
+        {
+            return data.SACHes.OrderByDescending(a => a.Ngaycapnhat).ToList();
+        }
+
+
+        public ActionResult Index()
+        {
+            var sachmoi = Laysachmoi(5);
+            return View(sachmoi);
+        }
+        
+
+        public ActionResult SubPage()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            var sach = from s in data.SACHes where s.Masach == id select s;
+            return PartialView(sach.Single());
+        }
+
+        public ActionResult Chude()
+        {
+            var chude = from cd in data.CHUDEs select cd;
+            return PartialView(chude);
+        }
+
+        public ActionResult Nhaxuatban()
+        {
+            var nxb = from cd in data.NHAXUATBANs select cd;
+            return PartialView(nxb);
+        }
+
+        public ActionResult SPTheochude(int id)
+        {
+            var sach = from s in data.SACHes where  s.MaCD==id select s;
+            return View(sach);
+        }
+
+        public ActionResult SPTheoNXB(int id)
+        {
+            var sach = from s in data.SACHes where s.MaNXB == id select s;
+            return PartialView(sach);
+        }
+    }
+}
